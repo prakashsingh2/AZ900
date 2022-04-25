b4f34dd9a794b2ef31789b62de1143afa0a89e42
prakashsingh2 <prakash.singh@abbvie.com>
2022-04-24 19:58:06 -0500

    Copy file from system to Azure File System

---
+++ BlobApp/BlobApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BlobApp
+{
+    class Program
+    {
+        static string str_connection = "DefaultEndpointsProtocol=https;AccountName=storageaccountps25;AccountKey=DLKTYHTdWM+tX+TG4MhYue6bnCe5MCzWn3rebR0p3wSbd94k9nvhDfQbDp+eR/rpy4dIKSPLcbRUt70bpFddqQ==;EndpointSuffix=core.windows.net";
+        static async Task Main()
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(str_connection);
+            BlobContainerClient container_client = blobServiceClient.GetBlobContainerClient("data");
+
+            BlobClient blob_client=container_client.GetBlobClient("debug.txt");
+            using FileStream uploadFileStream = File.OpenRead("C:\\temp\\debug.txt");
+            await blob_client.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
+            Console.WriteLine("File uploaded");
+
+            Console.WriteLine("Operation complete");
+        }
+    }
+}
